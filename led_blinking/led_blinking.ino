e975b863515087d94cd3feab713cdd8ebb061e6c
robojose <contact.robojose@gmail.com>
2026-05-16 01:57:51 -0400

    adding project 2: LED blinking

---
+++ led_blinking/led_blinking.ino
@@ -0,0 +1,16 @@
+//From keyestudio Basic Starter V2 Kit for Arduino, project 2: LED blinking
+int ledPin = 10; //define digital pin 10
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT); // define pin with LED connected as output. 
+}
+
+void loop()
+{
+
+  digitalWrite(ledPin,HIGH); //set the LED on.
+  delay(1000);;//wait for a second
+  digitalWrite(ledPin,LOW); //set the LED off.
+  delay(1000); //wait for a second
+}
